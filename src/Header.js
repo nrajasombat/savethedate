3af4c8822a95eb0f7cd1d19b11c4f0a0e4220f31
tharajasombat <n.rajasombat@gmail.com>
2019-01-16 22:31:22 -0800

    Add eslint npm

--- src/Header.js
+++ src/Header.js
@@ -1,46 +1,46 @@
-import React, {Component} from 'react';
-import {Nav, Navbar,NavItem} from 'react-bootstrap'
-import './App.css';
+import React, { Component } from "react";
+import { Nav, Navbar, NavItem } from "react-bootstrap";
+import "./App.css";
 
 class Header extends Component {
-    render() {
-        return (
-            <div className="Header">
-                <Navbar collapseOnSelect>
-                    <Navbar.Header>
-                        <Navbar.Brand>
-                            <a href="#brand">Tha & Vu</a>
-                        </Navbar.Brand>
-                        <Navbar.Toggle />
-                    </Navbar.Header>
-                    <Navbar.Collapse>
-                        <Nav>
-                            <NavItem eventKey={1} href="#">
-                                About
-                            </NavItem>
-                            <NavItem eventKey={2} href="#">
-                                Our Story
-                            </NavItem>
-                            <NavItem eventKey={3} href="#">
-                                Attendants
-                            </NavItem>
-                        </Nav>
-                        <Nav pullRight>
-                            <NavItem eventKey={1} href="#">
-                                The Event
-                            </NavItem>
-                            <NavItem eventKey={2} href="#">
-                                Registry
-                            </NavItem>
-                            <NavItem eventKey={3} href="#">
-                                Gallery
-                            </NavItem>
-                        </Nav>
-                    </Navbar.Collapse>
-                </Navbar>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div className="Header">
+        <Navbar collapseOnSelect>
+          <Navbar.Header>
+            <Navbar.Brand>
+              <a href="#brand">Tha & Vu</a>
+            </Navbar.Brand>
+            <Navbar.Toggle />
+          </Navbar.Header>
+          <Navbar.Collapse>
+            <Nav>
+              <NavItem eventKey={1} href="#">
+                About
+              </NavItem>
+              <NavItem eventKey={2} href="#">
+                Our Story
+              </NavItem>
+              <NavItem eventKey={3} href="#">
+                Attendants
+              </NavItem>
+            </Nav>
+            <Nav pullRight>
+              <NavItem eventKey={1} href="#">
+                The Event
+              </NavItem>
+              <NavItem eventKey={2} href="#">
+                Registry
+              </NavItem>
+              <NavItem eventKey={3} href="#">
+                Gallery
+              </NavItem>
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
+      </div>
+    );
+  }
 }
 
 export default Header;
--- src/Section.js
+++ src/Section.js
@@ -1,18 +1,17 @@
-import React, {Component} from 'react';
-import './Section.css';
+import React, { Component } from "react";
+import "./Section.css";
 
 class Section extends Component {
-    render() {
-        return (
-            <div class="container">
-                <div className="section-heading">
-                    <h2>About The Couple</h2>
-                    <div className="hr">
-                    </div>
-                </div>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div class="container">
+        <div className="section-heading">
+          <h2>About The Couple</h2>
+          <div className="hr" />
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Section;
