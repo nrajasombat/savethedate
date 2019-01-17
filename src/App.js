02ffcd95b1cae2e16d6176a1e3c85613a943a663
tharajasombat <n.rajasombat@gmail.com>
2019-01-16 22:18:24 -0800

    Add eslint

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import React, { Component } from 'react';
-import './App.css';
-import About from './About'
+import React, { Component } from "react";
+import "./App.css";
+import About from "./About";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <About/>
+        <About />
       </div>
     );
   }
