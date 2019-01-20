bad45068bcdc43920f8ab5e7e6f66c2e2dd36de3
tharajasombat <n.rajasombat@gmail.com>
2019-01-20 05:41:27 -0800

    Add Bridesmaid component and style, download Owl Carousel

--- src/Bridesmaid.js
+++ src/Bridesmaid.js
@@ -1,56 +1,56 @@
 import React, { Component } from "react";
-import "./About.css";
+import "./Bridesmaid.scss";
 import OwlCarousel from "react-owl-carousel";
 import "owl.carousel/dist/assets/owl.carousel.css";
 import "owl.carousel";
-import thapic from "./images/Tha.png";
+import aninapic from "./images/anina.png";
+import neenapic from "./images/Neena.png";
+import lisapic from "./images/Lisa.png";
+import jennypic from "./images/Jenny.png";
+import ashleypic from "./images/Ashl.png";
 import { Image } from "react-bootstrap";
 
-const bridesmaid = [{}];
+const bridesmaids = [
+  {
+    picture: jennypic,
+    name: "Jenny Manokoune",
+    job: "Maid of Honor"
+  },
+  {
+    picture: aninapic,
+    name: "Anina Rasaphangthong",
+    job: "Bridesmaid"
+  },
+  {
+    picture: neenapic,
+    name: "Neena Keoonla",
+    job: "Matron of Honor"
+  },
+  {
+    picture: lisapic,
+    name: "Lisa Keoonla",
+    job: "Bridesmaid"
+  },
+  {
+    picture: ashleypic,
+    name: "Ashley Ngaoluangrath",
+    job: "Bridesmaid"
+  }
+];
 
 class Bridesmaid extends Component {
   render() {
     return (
-      <div class="bridemaid">
-        <div className="container">
-          <OwlCarousel className="owl-theme" loop margin={10} nav>
-            <div class="item">
-              <Image src={thapic} circle bsClass="profile img" />
-              <div className="info">
-                <h5>Jenny Manokoune</h5>
-                <h4>Maid Of Honor</h4>
-              </div>
+      <div className="container">
+        <OwlCarousel className="owl-theme" loop margin={10} nav>
+          {bridesmaids.map(bridesmaid => (
+            <div class="bridalparty">
+              <Image src={bridesmaid.picture} circle bsClass="profile img" />
+              <div className="info">{bridesmaid.name}</div>
+              <div className="description">{bridesmaid.job}</div>
             </div>
-            <div class="item">
-              <Image src={thapic} circle bsClass="profile img" />
-              <div className="info">
-                <h5>Jenny Manokoune</h5>
-                <h4>Maid Of Honor</h4>
-              </div>
-            </div>
-            <div class="item">
-              <Image src={thapic} circle bsClass="profile img" />
-              <div className="info">
-                <h5>Jenny Manokoune</h5>
-                <h4>Maid Of Honor</h4>
-              </div>
-            </div>
-            <div class="item">
-              <Image src={thapic} circle bsClass="profile img" />
-              <div className="info">
-                <h5>Jenny Manokoune</h5>
-                <h4>Maid Of Honor</h4>
-              </div>
-            </div>
-            <div class="item">
-              <Image src={thapic} circle bsClass="profile img" />
-              <div className="info">
-                <h5>Jenny Manokoune</h5>
-                <h4>Maid Of Honor</h4>
-              </div>
-            </div>
-          </OwlCarousel>
-        </div>
+          ))}
+        </OwlCarousel>
       </div>
     );
   }
