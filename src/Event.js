70e9e2494ec47c3f74789e8d8ab3bfa8631c34d9
tharajasombat <n.rajasombat@gmail.com>
2019-01-22 14:32:55 -0800

    Add Event Component with Location and style

--- src/Event.js
+++ src/Event.js
@@ -62,6 +62,86 @@ class Event extends Component {
               </div>
             </Col>
           </Row>
+          <Row>
+            <Col md={6}>
+              <div class="mapouter">
+                <div class="gmap_canvas">
+                  <iframe
+                    width="600px"
+                    height="450px"
+                    id="gmap_canvas"
+                    src="https://maps.google.com/maps?q=dallas%20palms&t=&z=13&ie=UTF8&iwloc=&output=embed"
+                    frameborder="0"
+                    scrolling="no"
+                    marginheight="0"
+                    marginwidth="0"
+                  />
+                  <a href="https://www.embedgooglemap.net" />
+                </div>
+              </div>
+            </Col>
+            <Col md={6}>
+              <div className="margin1">
+                <div className="right">
+                  <div className="title">Ceremony</div>
+                  <p>
+                    Imperdiet interdum donec eget metus auguen unc vel lorem
+                    ispuet Ibu lum orci eget, viverra elit liquam erat volut pat
+                    phas ellus ac sodales Lorem ipsum dolor sit amet,
+                    consectetur adipisicing elit uasi quidem minus id iprum
+                    omnis. Lorem ipsum dolor Phas ellus ac sodales felis tiam.
+                  </p>
+                  <p>
+                    Imperdiet interdum donec eget metus auguen unc vel lorem
+                    ispuet Ibu lum orci eget, viverra elit liquam erat volut pat
+                    phas ellus ac sodales Lorem ipsum dolor sit amet,
+                    consectetur adipisicing elit uasi quidem minus id iprum
+                    omnis. Lorem ipsum dolor Phas ellus ac sodales felis tiam.
+                  </p>
+                </div>
+              </div>
+            </Col>
+          </Row>
+          <Row>
+            <Col md={6}>
+              <div className="margin1">
+                <div className="right">
+                  <div className="title">Reception</div>
+                  <p>
+                    Imperdiet interdum donec eget metus auguen unc vel lorem
+                    ispuet Ibu lum orci eget, viverra elit liquam erat volut pat
+                    phas ellus ac sodales Lorem ipsum dolor sit amet,
+                    consectetur adipisicing elit uasi quidem minus id iprum
+                    omnis. Lorem ipsum dolor Phas ellus ac sodales felis tiam.
+                  </p>
+                  <p>
+                    Imperdiet interdum donec eget metus auguen unc vel lorem
+                    ispuet Ibu lum orci eget, viverra elit liquam erat volut pat
+                    phas ellus ac sodales Lorem ipsum dolor sit amet,
+                    consectetur adipisicing elit uasi quidem minus id iprum
+                    omnis. Lorem ipsum dolor Phas ellus ac sodales felis tiam.
+                  </p>
+                </div>
+              </div>
+            </Col>
+            <Col md={6}>
+              <div class="mapouter">
+                <div class="gmap_canvas">
+                  <iframe
+                    width="600"
+                    height="450"
+                    id="gmap_canvas"
+                    src="https://maps.google.com/maps?q=Kirin%20Court&t=&z=13&ie=UTF8&iwloc=&output=embed"
+                    frameborder="0"
+                    scrolling="no"
+                    marginheight="0"
+                    marginwidth="0"
+                  />
+                  <a href="https://www.embedgooglemap.net" />
+                </div>
+              </div>
+            </Col>
+          </Row>
         </div>
       </div>
     );
