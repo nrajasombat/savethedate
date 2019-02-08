54abf8a3f36290203e22a2d8314c824f16450bd0
tharajasombat <n.rajasombat@gmail.com>
2019-02-08 15:28:27 -0800

    Add Detail component to app

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import About from './About';
 import Attendants from './Attendants';
 import Event from './Event';
 import OurStory from './OurStory';
+import Details from './Details';
 
 class App extends Component {
   render() {
@@ -13,6 +14,7 @@ class App extends Component {
         <OurStory />
         <Attendants />
         <Event />
+        <Details />
       </div>
     );
   }
---
+++ src/Details.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import { Col, Row } from 'react-bootstrap';
+import './Event.scss';
+const Details = () => {
+  return (
+    <div class="event">
+      <div className="container">
+        <Row>
+          <Col md={12}>
+            <div className="right">
+              <div className="title">Details</div>
+              <p>
+                Our reception may be a little different from what you are used
+                to. Here are some things that may be unfamiliar:
+                <li>
+                  When you arrive we will be waiting to take pictures with you.
+                  Cheese!
+                </li>
+                <li>
+                  The reception meal will be served family style, so be ready to
+                  share with your table-mates.
+                </li>
+                <li>
+                  Cash or checks are preferred for our wedding. These are
+                  typically given to the bride and groom in hongbaos (red
+                  envelopes) or whatever envelopes you have at home. But if you
+                  prefer we still have a registry! And any personal gifts will
+                  be loved and cherished.
+                </li>
+                <li>
+                  Towards the end of the meal, the wedding party will visit each
+                  table to give everyone a chance to celebrate. During this time
+                  it would be best to select individual from the table to
+                  collect the hongbaos/envelopes to present to the bride and
+                  groom, and say a few words that will be remembered forever so
+                  no pressure.
+                </li>
+              </p>
+            </div>
+          </Col>
+        </Row>
+      </div>
+    </div>
+  );
+};
+
+export default Details;
