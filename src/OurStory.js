f379a46c74a057229dc8313f50ba6eddb160f4da
tharajasombat <n.rajasombat@gmail.com>
2019-02-19 02:38:43 -0800

    Add StagePadding to OwlCarousel

--- src/OurStory.js
+++ src/OurStory.js
@@ -9,74 +9,135 @@ import pic3 from './images/brianwedding.png';
 import pic4 from './images/sefwedding.png';
 import pic5 from './images/xmas2016.png';
 import pic6 from './images/calitrip.png';
-import { Image } from 'react-bootstrap';
+import { Image, Row } from 'react-bootstrap';
 
 const selfies = [
   {
     picture: pic1,
-    description: 'Jenny Manokoune',
+    description: '2012',
     date: 'Maid of Honor',
   },
   {
     picture: pic2,
-    description: 'Jenny Manokoune',
+    description: '2013',
     date: 'Maid of Honor',
   },
   {
     picture: pic3,
-    description: 'Jenny Manokoune',
+    description: '2013',
     date: 'Maid of Honor',
   },
   {
     picture: pic4,
-    description: 'Jenny Manokoune',
+    description: '2014',
     date: 'Maid of Honor',
   },
   {
     picture: pic5,
-    description: 'Jenny Manokoune',
+    description: '2015',
     date: 'Maid of Honor',
   },
   {
     picture: pic6,
-    description: 'Jenny Manokoune',
+    description: '2015',
     date: 'Maid of Honor',
   },
 ];
 const options = {
-  items: '4',
-  nav: 'true',
-  loop: 'true',
+  loop: true,
+  margin: 10,
+  nav: true,
+  lazyLoad: true,
+
   responsive: {
-    0: { items: 1 },
-    425: { items: 1 },
-    800: { items: 3 },
+    0: {
+      items: 1,
+      stagePadding: 50,
+    },
+    300: {
+      items: 1,
+      stagePadding: 50,
+    },
+    500: {
+      items: 1,
+      stagePadding: 90,
+    },
+    600: {
+      items: 1,
+      stagePadding: 150,
+    },
+
+    700: {
+      items: 1,
+      stagePadding: 200,
+    },
+    800: {
+      items: 1,
+      stagePadding: 250,
+    },
+    1000: {
+      items: 2,
+      stagePadding: 150,
+    },
+
+    1200: {
+      items: 3,
+      stagePadding: 150,
+    },
+    1400: {
+      items: 3,
+      stagePadding: 150,
+    },
+    1600: {
+      items: 3,
+      stagePadding: 150,
+    },
+    1800: {
+      items: 3,
+      stagePadding: 150,
+    },
+    2000: {
+      items: 4,
+      stagePadding: 250,
+    },
+    3000: {
+      items: 5,
+      stagePadding: 250,
+    },
+    4000: {
+      items: 5,
+      stagePadding: 250,
+    },
   },
 };
 
 const OurStory = () => {
   return (
     <div className="ourStory">
-      <div className="container">
+      <div className="container-fluid">
         <div className="section-heading">
           <h2>Our Story</h2>
           <div className="heart" />
-          <OwlCarousel className="owl-theme" margin={10} {...options}>
-            {selfies.map(selfie => (
-              <div class="photos">
-                <a href={selfie.picture}>
-                  <Image
-                    src={selfie.picture}
-                    rounded
-                    responsive
-                    bsClass="selfies img"
-                  />
-                  <h1>{selfie.description}</h1>
-                  <p>{selfie.date}</p>
-                </a>
-              </div>
-            ))}
-          </OwlCarousel>
+          <Row>
+            <OwlCarousel className="owl-theme" {...options}>
+              {selfies.map(selfie => (
+                <div class="photos">
+                  <a href={selfie.picture}>
+                    <Image
+                      src={selfie.picture}
+                      rounded
+                      responsive
+                      bsClass="selfies img"
+                    />
+                    <div className="whenwemet">
+                      <span>{selfie.description}</span>
+                      <p>{selfie.date}</p>
+                    </div>
+                  </a>
+                </div>
+              ))}
+            </OwlCarousel>
+          </Row>
         </div>
       </div>
     </div>
