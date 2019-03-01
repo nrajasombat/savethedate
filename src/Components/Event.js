4e1f3ece8bebefc990fc3d9f64ff7523cc060341
tharajasombat <n.rajasombat@gmail.com>
2019-03-01 14:23:35 -0800

    Add pics to event images

--- src/Components/Event.js
+++ src/Components/Event.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Col, Image, Row } from 'react-bootstrap';
-import celebrate from '../images/Celebrate.png';
+import ballroom from '../images/events/ballroom.png';
+import dallaspalms from '../images/events/dallaspalms.png';
 import '../Event.scss';
 
 const Event = ({ title }) => {
@@ -13,20 +14,15 @@ const Event = ({ title }) => {
           <Row>
             <Col md={6}>
               <div className="left">
-                <Image src={celebrate} rounded bsClass="celebrate img" />
+                <Image src={dallaspalms} rounded bsClass="celebrate img" />
               </div>
             </Col>
             <Col md={6}>
               <div className="right">
                 <div className="title">Celebrate With Us</div>
-                <p>Saturday, September 2, 2017 Time: 3:00 PM Attire: Formal</p>
-                <p>
-                  Imperdiet interdum donec eget metus auguen unc vel lorem
-                  ispuet Ibu lum orci eget, viverra elit liquam erat volut pat
-                  phas ellus ac sodales Lorem ipsum dolor sit amet, consectetur
-                  adipisicing elit uasi quidem minus id iprum omnis. Lorem ipsum
-                  dolor Phas ellus ac sodales felis tiam.
-                </p>
+                <div> Date: Saturday, October 26, 2018 </div>
+                <div> Time: 3:00 PM </div>
+                <div>Attire: Formal</div>
               </div>
             </Col>
           </Row>
@@ -34,32 +30,19 @@ const Event = ({ title }) => {
             <Col md={6}>
               <div className="margin1">
                 <div className="right">
-                  <div className="title">Celebrate With Us</div>
-                  <p>
-                    Imperdiet interdum donec eget metus auguen unc vel lorem
-                    ispuet Ibu lum orci eget, viverra elit liquam erat volut pat
-                    phas ellus ac sodales Lorem ipsum dolor sit amet,
-                    consectetur adipisicing elit uasi quidem minus id iprum
-                    omnis. Lorem ipsum dolor Phas ellus ac sodales felis tiam.
-                  </p>
-                  <p>
-                    Imperdiet interdum donec eget metus auguen unc vel lorem
-                    ispuet Ibu lum orci eget, viverra elit liquam erat volut pat
-                    phas ellus ac sodales Lorem ipsum dolor sit amet,
-                    consectetur adipisicing elit uasi quidem minus id iprum
-                    omnis. Lorem ipsum dolor Phas ellus ac sodales felis tiam.
-                  </p>
+                  <div className="title">The Ceremony</div>
+                  <h6> Dallas Palms</h6>
+                  <h6>2424 Marsh Lane</h6>
+                  <h6>Carrollton, Texas 75006</h6>
                 </div>
               </div>
             </Col>
             <Col md={6}>
               <div className="left">
-                <Image src={celebrate} rounded bsClass="celebrate img" />
+                <Image src={ballroom} rounded bsClass="celebrate img" />
               </div>
             </Col>
-          </Row>
-          <Row>
-            <Col md={6}>
+            <Col md={12}>
               <div className="resp-container">
                 <iframe
                   class="resp-iframe"
@@ -72,31 +55,30 @@ const Event = ({ title }) => {
                 />
               </div>
             </Col>
-            <Col md={6}>
-              <div className="margin1">
-                <div className="right">
-                  <div className="title">Ceremony</div>
-                  <h2>October 26, 2019</h2>
-                  <h6> Dallas Palms</h6>
-                  <h6> Time: 3:00 PM</h6>
-                  <h6>2424 Marsh Ln, Carrollton, TX 75006</h6>
-                </div>
-              </div>
-            </Col>
           </Row>
           <Row>
             <Col md={6}>
               <div className="margin1">
                 <div className="right">
-                  <div className="title">Reception</div>
-                  <h2>October 26, 2019</h2>
-                  <h6> Kirin Court</h6>
+                  <div className="title"> The Reception</div>
+                  <div> Date: Saturday, October 26, 2018 </div>
                   <h6> Time: 6:00 PM - Midnight</h6>
-                  <h6>221 W Polk St #200, Richardson, TX 75081</h6>
+                  <h6> Kirin Court</h6>
+                  <h6>221 W Polk St #200</h6>
+                  <h6> Richardson, TX 75081</h6>
                 </div>
               </div>
             </Col>
             <Col md={6}>
+              <div className="margin1">
+                <div className="left">
+                  <Image src={dallaspalms} rounded bsClass="celebrate img" />
+                </div>
+              </div>
+            </Col>
+          </Row>
+          <Row>
+            <Col md={12}>
               <div className="resp-container">
                 <iframe
                   class="resp-iframe"
