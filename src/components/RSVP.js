478b968068ba2ee729c948ecd9ba9639251a8307
tharajasombat <n.rajasombat@gmail.com>
2019-07-08 20:41:04 -0700

    Add RSVP with Google Form

--- src/components/RSVP.js
+++ src/components/RSVP.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { Row, Col, Image } from 'react-bootstrap';
+import savethedate from '../images/ourstory/savedate.png';
 
 const RSVP = ({ title }) => {
   return (
@@ -7,7 +9,18 @@ const RSVP = ({ title }) => {
         <div className="section-heading">
           <h2>{title}</h2>
           <div className="heart" />
-          Hello
+          <Row>
+            <Col md={12}>
+              <div className="rsvp-icon">
+                <a
+                  href=" https://docs.google.com/forms/d/e/1FAIpQLSev8Jz13NEmzq_0KRpbwVnTEp8h-T1zQ4n1KUfCgpEqvimL-A/viewform?usp=sf_link"
+                  target="_blank"
+                >
+                  <Image src={savethedate} />
+                </a>
+              </div>
+            </Col>
+          </Row>
         </div>
       </div>
     </div>
