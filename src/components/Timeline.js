036c9a1e7a0092d9020365c4863b3a0534387f7b
tharajasombat <n.rajasombat@gmail.com>
2019-07-22 15:37:30 -0700

    Add Style to Timeline Component

--- src/components/Timeline.js
+++ src/components/Timeline.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Row, Col } from 'react-bootstrap';
+import '../style/Timeline.scss';
 
 const Timeline = ({ title }) => {
   return (
@@ -8,78 +8,92 @@ const Timeline = ({ title }) => {
         <div className="section-heading">
           <h2>{title}</h2>
           <div className="heart" />
-          <Row>
-            <Col md={12}>
-              <div class="timeline">
-                <div class="container left">
-                  <div class="content">
-                    <h2>2017</h2>
-                    <p>
-                      Lorem ipsum dolor sit amet, quo ei simul congue exerci, ad
-                      nec admodum perfecto mnesarchum, vim ea mazim fierent
-                      detracto. Ea quis iuvaret expetendis his, te elit voluptua
-                      dignissim per, habeo iusto primis ea eam.
-                    </p>
-                  </div>
-                </div>
-                <div class="container right">
-                  <div class="content">
-                    <h2>2016</h2>
-                    <p>
-                      Lorem ipsum dolor sit amet, quo ei simul congue exerci, ad
-                      nec admodum perfecto mnesarchum, vim ea mazim fierent
-                      detracto. Ea quis iuvaret expetendis his, te elit voluptua
-                      dignissim per, habeo iusto primis ea eam.
-                    </p>
-                  </div>
-                </div>
-                <div class="container left">
-                  <div class="content">
-                    <h2>2015</h2>
-                    <p>
-                      Lorem ipsum dolor sit amet, quo ei simul congue exerci, ad
-                      nec admodum perfecto mnesarchum, vim ea mazim fierent
-                      detracto. Ea quis iuvaret expetendis his, te elit voluptua
-                      dignissim per, habeo iusto primis ea eam.
-                    </p>
-                  </div>
-                </div>
-                <div class="container right">
-                  <div class="content">
-                    <h2>2012</h2>
-                    <p>
-                      Lorem ipsum dolor sit amet, quo ei simul congue exerci, ad
-                      nec admodum perfecto mnesarchum, vim ea mazim fierent
-                      detracto. Ea quis iuvaret expetendis his, te elit voluptua
-                      dignissim per, habeo iusto primis ea eam.
-                    </p>
-                  </div>
-                </div>
-                <div class="container left">
-                  <div class="content">
-                    <h2>2011</h2>
-                    <p>
-                      Lorem ipsum dolor sit amet, quo ei simul congue exerci, ad
-                      nec admodum perfecto mnesarchum, vim ea mazim fierent
-                      detracto. Ea quis iuvaret expetendis his, te elit voluptua
-                      dignissim per, habeo iusto primis ea eam.
-                    </p>
-                  </div>
-                </div>
-                <div class="container right">
-                  <div class="content">
-                    <h2>2007</h2>
-                    <p>
-                      Lorem ipsum dolor sit amet, quo ei simul congue exerci, ad
-                      nec admodum perfecto mnesarchum, vim ea mazim fierent
-                      detracto. Ea quis iuvaret expetendis his, te elit voluptua
-                      dignissim per, habeo iusto primis ea eam.
-                    </p>
-                  </div>
-                </div>
+          <section id="cd-timeline" class="cd-container">
+            <div className="cd-timeline-block">
+              <div className="cd-timeline-img cd-picture"></div>
+
+              <div className="cd-timeline-content">
+                <h2>Penta Consulting</h2>
+                <p>
+                  Working alongside the designer team implementing the designs,
+                  also developing custom solutions to address team necessities.
+                </p>
               </div>
-            </Col>
-          </Row>
+            </div>
+
+            <div className="cd-timeline-block">
+              <div className="cd-timeline-img cd-movie"></div>
+
+              <div className="cd-timeline-content">
+                <h2>Title of section 2</h2>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                  Iusto, optio, dolorum provident rerum aut hic quasi placeat
+                  iure tempora laudantium ipsa ad debitis unde?
+                </p>
+                <span className="cd-date">Jan 18</span>
+              </div>
+            </div>
+
+            <div className="cd-timeline-block">
+              <div className="cd-timeline-img cd-picture"></div>
+
+              <div className="cd-timeline-content">
+                <h2>Title of section 3</h2>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                  Excepturi, obcaecati, quisquam id molestias eaque asperiores
+                  voluptatibus cupiditate error assumenda delectus odit
+                  similique earum voluptatem doloremque dolorem ipsam quae rerum
+                  quis. Odit, itaque, deserunt corporis vero ipsum nisi eius
+                  odio natus ullam provident pariatur temporibus quia eos
+                  repellat consequuntur perferendis enim amet quae quasi
+                  repudiandae sed quod veniam dolore possimus rem voluptatum
+                  eveniet eligendi quis fugiat aliquam sunt similique aut
+                  adipisci.
+                </p>
+                <div className="cd-date">Jan 24</div>
+              </div>
+            </div>
+
+            <div className="cd-timeline-block">
+              <div className="cd-timeline-img cd-location"></div>
+
+              <div className="cd-timeline-content">
+                <h2>Title of section 4</h2>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                  Iusto, optio, dolorum provident rerum aut hic quasi placeat
+                  iure tempora laudantium ipsa ad debitis unde? Iste
+                  voluptatibus minus veritatis qui ut.
+                </p>
+                <span className="cd-date">Feb 14</span>
+              </div>
+            </div>
+
+            <div className="cd-timeline-block">
+              <div className="cd-timeline-img cd-location"></div>
+
+              <div className="cd-timeline-content">
+                <h2>Title of section 5</h2>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                  Iusto, optio, dolorum provident rerum.
+                </p>
+                <span className="cd-date">Feb 18</span>
+              </div>
+            </div>
+
+            <div className="cd-timeline-block">
+              <div className="cd-timeline-img cd-movie"></div>
+
+              <div class="cd-timeline-content">
+                <h2>Final Section</h2>
+                <p>This is the content of the last section</p>
+                <span class="cd-date">Feb 26</span>
+              </div>
+            </div>
+          </section>
         </div>
       </div>
     </div>
